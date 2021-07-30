29686f1090550a823cb870e68daf511b5071a549
Nekurone <zx627924@ou.ac.uk>
2021-07-30 10:52:36 +0100

    Clean up some of the variable names

--- double_pendulum.py
+++ double_pendulum.py
@@ -6,23 +6,20 @@ import sys, argparse, textwrap
 
 # Not clean, but it works
 # Board drawing functions
-
-
-def draw_point(scr: curses.window, A: int, B: int, c: str):
-    if A < 0 or B < 0 or A >= WIDTH / dW or B >= HEIGHT / dH:
-        scr.addstr(6, 0, "Hello.")
+def draw_point(screen: curses.window, A: int, B: int, c: str):
+    if A < 0 or B < 0 or A >= WIDTH / d_WIDTH or B >= HEIGHT / d_HEIGHT:
         return
-    scr.addstr(int(B), int(A), c)
+    screen.addstr(int(B), int(A), c)
 
 
-def draw_line(scr: curses.window, A: float, B: float, C: float, D: float, c: str):
+def draw_line(screen: curses.window, A: float, B: float, C: float, D: float, c: str):
 
     if A > C:
         C, A = A, C
         D, B = B, D
     if B == D:
         for i in range(int(A), int(C) + 1):
-            draw_point(scr, i, B, c)
+            draw_point(screen, i, B, c)
 
     if A == C:
         mi = B
@@ -31,18 +28,18 @@ def draw_line(scr: curses.window, A: float, B: float, C: float, D: float, c: str
             mi = D
             ma = B
         for i in range(int(mi), int(ma) + 1):
-            draw_point(scr, A, i, c)
+            draw_point(screen, A, i, c)
     if abs(D - B) < abs(C - A):
-        plot_line_low(scr, A, B, C, D, c)
+        plot_line_low(screen, A, B, C, D, c)
     else:
         if B > D:
-            plot_line_high(scr, C, D, A, B, c)
+            plot_line_high(screen, C, D, A, B, c)
         else:
-            plot_line_high(scr, A, B, C, D, c)
+            plot_line_high(screen, A, B, C, D, c)
 
 
 def plot_line_low(
-    scr: curses.window, x0: float, y0: float, x1: float, y1: float, c: str
+    screen: curses.window, x0: float, y0: float, x1: float, y1: float, c: str
 ):
     dx = x1 - x0
     dy = y1 - y0
@@ -53,16 +50,16 @@ def plot_line_low(
     D = 2 * dy - dx
     y = y0
     for x in range(int(x0), int(x1) + 1):
-        draw_point(scr, x, y, c)
+        draw_point(screen, x, y, c)
         if D > 0:
             y += yi
             D -= 2 * dx
         D += 2 * dy
-        # scr.addstr(0,0,'f1')
+        # screen.addstr(0,0,'f1')
 
 
 def plot_line_high(
-    scr: curses.window, x0: float, y0: float, x1: float, y1: float, c: str
+    screen: curses.window, x0: float, y0: float, x1: float, y1: float, c: str
 ):
 
     dx = x1 - x0
@@ -76,7 +73,7 @@ def plot_line_high(
     x = x0
 
     for y in range(int(y0), int(y1) + 1):
-        draw_point(scr, x, y, c)
+        draw_point(screen, x, y, c)
         if D > 0:
             x += xi
             D -= 2 * dy
@@ -86,38 +83,38 @@ def plot_line_high(
 # Number of Pendulums to Simulate.
 epsilon = 0.00001
 g = 9.81  # Pfft, screw gravity.
-l1, l2 = {}, {}  # Lengths
-m1, m2 = {}, {}  # Masses
+length_1, length_2 = {}, {}  # Lengths
+mass_1, mass_2 = {}, {}  # Masses
 O1, O2 = {}, {}  # Angles
-w1, w2 = {}, {}  # Angular Velocities
+omega_1, omega_2 = {}, {}  # Angular Velocities
 
 # Screen parameters
 WIDTH = 1440
 HEIGHT = 1200
-dW = 14
-dH = 40
+d_WIDTH = 14
+d_HEIGHT = 40
 
 
-def sim(n: int, trace: bool, length: float, mass: float):
-    for i in range(n):
+def sim(no_of_pendulums: int, trace: bool, length: float, mass: float):
+    for i in range(no_of_pendulums):
         # Define them all manually for that sweet chaos
         # Length
-        l1[i] = length
-        l2[i] = length
+        length_1[i] = length
+        length_2[i] = length
         # Mass
-        m1[i] = mass
-        m2[i] = mass
+        mass_1[i] = mass
+        mass_2[i] = mass
         # Original angle to be offset slightly
 
-        O1[i] = 2.0 * pi / 2.0 + epsilon * (float(2 * i - n + 4))
+        O1[i] = 2.0 * pi / 2.0 + epsilon * (float(2 * i - no_of_pendulums + 4))
         O2[i] = 2.0 * pi / 2.0
         ## Angular Velocity
-        w1[i] = 0.0
-        w2[i] = 0.0
+        omega_1[i] = 0.0
+        omega_2[i] = 0.0
 
     fps = 300.0
     dt = 1.0 / fps
-    acc = 0.0
+    accumulator = 0.0
     frame_start = datetime.now()
     # Actually initialise window Now
     stdscr = curses.initscr()
@@ -126,62 +123,74 @@ def sim(n: int, trace: bool, length: float, mass: float):
 
     # Initialise board
     if trace:
-        trace = [[0] * int(WIDTH / dW) for x in range(int(HEIGHT / dH))]
-    # stdscr.addstr(floor(HEIGHT/dH)-1,floor(WIDTH/dW),'\0')
-    # for x in range(floor(HEIGHT/dH)-1):
-    #    stdscr.addstr(i,floor(WIDTH/dW),'\n')
+        trace = [[0] * int(WIDTH / d_WIDTH) for x in range(int(HEIGHT / d_HEIGHT))]
     f = 0
 
     while True:
         f += 1
         current = datetime.now()
-        acc += (current - frame_start) / timedelta(microseconds=1)
+        accumulator += (current - frame_start) / timedelta(microseconds=1)
         frame_start = current
-        if acc >= 1.0 / 30.0:
-            acc = 1.0 / 30.0
-        while acc > dt:
-            for i in range(n):
+        if accumulator >= 1.0 / 30.0:
+            accumulator = 1.0 / 30.0
+        while accumulator > dt:
+            for i in range(no_of_pendulums):
                 a1 = (
-                    -g * (2 * m1[i] + m2[i]) * sin(O1[i])
-                    - g * m2[i] * sin(O1[i] - 2 * O2[i])
+                    -g * (2 * mass_1[i] + mass_2[i]) * sin(O1[i])
+                    - g * mass_2[i] * sin(O1[i] - 2 * O2[i])
                     - 2
-                    * m2[i]
+                    * mass_2[i]
                     * sin(O1[i] - O2[i])
                     * (
-                        w2[i] * w2[i] * l2[i]
-                        + w1[i] * w1[i] * l1[i] * cos(O1[i] - O2[i])
+                        omega_2[i] * omega_2[i] * length_2[i]
+                        + omega_1[i] * omega_1[i] * length_1[i] * cos(O1[i] - O2[i])
                     )
-                ) / (l1[i] * (2 * m1[i] + m2[i] - m2[i] * cos(2 * O1[i] - 2 * O2[i])))
+                ) / (
+                    length_1[i]
+                    * (
+                        2 * mass_1[i]
+                        + mass_2[i]
+                        - mass_2[i] * cos(2 * O1[i] - 2 * O2[i])
+                    )
+                )
                 a2 = (
                     (2 * sin(O1[i] - O2[i]))
                     * (
-                        w1[i] * w1[i] * l1[i] * (m1[i] + m2[i])
-                        + g * (m1[i] + m2[i]) * cos(O1[i])
-                        + w2[i] * w2[i] * l2[i] * m2[i] * cos(O1[i] - O2[i])
+                        omega_1[i] * omega_1[i] * length_1[i] * (mass_1[i] + mass_2[i])
+                        + g * (mass_1[i] + mass_2[i]) * cos(O1[i])
+                        + omega_2[i]
+                        * omega_2[i]
+                        * length_2[i]
+                        * mass_2[i]
+                        * cos(O1[i] - O2[i])
                     )
-                    / l2[i]
-                    / (2 * m1[i] + m2[i] - m2[i] * cos(2 * O1[i] - 2 * O2[i]))
+                    / length_2[i]
+                    / (
+                        2 * mass_1[i]
+                        + mass_2[i]
+                        - mass_2[i] * cos(2 * O1[i] - 2 * O2[i])
+                    )
                 )
                 ## Just trust them.
-                w1[i] += 15 * dt * a1
-                w2[i] += 15 * dt * a2
-                O1[i] += 15 * w1[i] * dt
-                O2[i] += 15 * w2[i] * dt
-            acc -= dt
+                omega_1[i] += 15 * dt * a1
+                omega_2[i] += 15 * dt * a2
+                O1[i] += 15 * omega_1[i] * dt
+                O2[i] += 15 * omega_2[i] * dt
+            accumulator -= dt
             if trace:
-                for i in range(int(HEIGHT / dH)):
-                    for j in range(int(WIDTH / dW)):
+                for i in range(int(HEIGHT / d_HEIGHT)):
+                    for j in range(int(WIDTH / d_WIDTH)):
                         if trace[i][j] > 0:
                             trace[i][j] -= 1
         if not trace:
             stdscr.clear()
-        for i in range(floor(HEIGHT / dH)):
-            # for j in range(floor(WIDTH/dW)):
+        for i in range(floor(HEIGHT / d_HEIGHT)):
+            # for j in range(floor(WIDTH/d_WIDTH)):
             #        stdscr.addstr(i,j,' ')
-            for j in range(floor(WIDTH / dW)):
+            for j in range(floor(WIDTH / d_WIDTH)):
                 if trace:
                     if stdscr.inch(i, j) == ord("@"):
-                        #stdscr.addstr(6, 0, "Tracing")
+                        # stdscr.addstr(6, 0, "Tracing")
                         trace[i][j] = fps
                     if trace[i][j] >= 3 * int(fps / 4):
                         stdscr.addstr(i, j, ":")
@@ -194,31 +203,37 @@ def sim(n: int, trace: bool, length: float, mass: float):
                             stdscr.addstr(i, j, " ")
                     else:
                         stdscr.addstr(i, j, " ")
-                if i < int(HEIGHT / dH / 2):
-                    draw_point(stdscr, WIDTH / 2 / dW, i, "|")
-        for i in range(n):
-            x1 = (WIDTH / 2 + sin(O1[i]) * l1[i] + dW * 0.5) / dW
-            y1 = (cos(O1[i]) * l1[i] + dH * 0.5) / dH + HEIGHT / dH / 2
-            x2 = x1 + (sin(O2[i]) * l2[i] + dW * 0.5) / dW
-            y2 = y1 + (cos(O2[i]) * l2[i] + dH * 0.5) / dH
+                if i < int(HEIGHT / d_HEIGHT / 2):
+                    draw_point(stdscr, WIDTH / 2 / d_WIDTH, i, "|")
+        for i in range(no_of_pendulums):
+            x1 = (WIDTH / 2 + sin(O1[i]) * length_1[i] + d_WIDTH * 0.5) / d_WIDTH
+            y1 = (
+                cos(O1[i]) * length_1[i] + d_HEIGHT * 0.5
+            ) / d_HEIGHT + HEIGHT / d_HEIGHT / 2
+            x2 = x1 + (sin(O2[i]) * length_2[i] + d_WIDTH * 0.5) / d_WIDTH
+            y2 = y1 + (cos(O2[i]) * length_2[i] + d_HEIGHT * 0.5) / d_HEIGHT
             if i % 2 == 0:
-                draw_line(stdscr, WIDTH / 2 / dW, HEIGHT / dH / 2, x1, y1, "*")
+                draw_line(
+                    stdscr, WIDTH / 2 / d_WIDTH, HEIGHT / d_HEIGHT / 2, x1, y1, "*"
+                )
                 draw_line(stdscr, x1, y1, x2, y2, "*")
                 # stdscr.addstr(0,0,'{} {} {} {}'.format(x1,y1,x2,y2))
-                draw_point(stdscr, WIDTH / 2 / dW, HEIGHT / dH / 2, "O")
+                draw_point(stdscr, WIDTH / 2 / d_WIDTH, HEIGHT / d_HEIGHT / 2, "O")
                 draw_point(stdscr, x1, y1, "@")
                 draw_point(stdscr, x2, y2, "@")
             else:
-                draw_line(stdscr, WIDTH / 2 / dW, HEIGHT / dH / 2, x1, y1, ".")
+                draw_line(
+                    stdscr, WIDTH / 2 / d_WIDTH, HEIGHT / d_HEIGHT / 2, x1, y1, "."
+                )
                 draw_line(stdscr, x1, y1, x2, y2, ".")
-                draw_point(stdscr, WIDTH / 2 / dW, HEIGHT / dH / 2, "O")
+                draw_point(stdscr, WIDTH / 2 / d_WIDTH, HEIGHT / d_HEIGHT / 2, "O")
                 draw_point(stdscr, x1, y1, "@")
                 draw_point(stdscr, x2, y2, "@")
         stdscr.addstr(
             0,
             0,
             "Flo's Double Pendulum. Number of Pendulums: {0},Time:{1}.".format(
-                n, floor(f / 30)
+                no_of_pendulums, floor(f / 30)
             ),
         )
         stdscr.refresh()
@@ -275,7 +290,7 @@ def main(argv):
             *OPTIONS
         )
     )
-    sleep(2)
+    sleep(3ß)
     sim(*OPTIONS)
 
 
