ff7f1d1935c421ff56cb47fac5d272bad9ae91ca
Nekurone <zx627924@ou.ac.uk>
2021-08-02 00:38:33 +0100

    Finally finish setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md",'r') as ld:
 
 setuptools.setup(
     name="double-pendulum",
-    version="1.0.3",
+    version="1.0.12",
     author="Nekurone",
     author_email="florencesecure@protonmail.com",
     description="A small double pendulum simulator",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Nekurone/double-pendulum-ascii/",
-    packages=setuptools.find_packages(),
+    py_modules = ["pendulum"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
@@ -20,4 +20,10 @@ setuptools.setup(
         "Intended Audience :: End Users/Desktop",
         "Topic :: Terminals"
     ],
+    entry_points = {
+        "console_scripts":[
+            "double-pendulum=pendulum:main"
+        ]
+    },
+    python_requires=">=3.6",
 )
