b8cf4de60a7767b06adfc814ba9ea6b801245369
Nekurone <zx627924@ou.ac.uk>
2022-06-09 13:45:35 +0100

    Black formatting

--- grabsys.py
+++ grabsys.py
@@ -5,66 +5,77 @@ import socket
 
 # psutil is the big one.
 
+
 def process(command: str):
-    return subprocess.Popen(
-        command,
-        shell=True,
-        stdout=subprocess.PIPE
-        ).communicate()[0].decode('utf-8')
+    return (
+        subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        .communicate()[0]
+        .decode("utf-8")
+    )
+
 
 def grab_res():
-    if platform.system() == 'Darwin':
-        '''
+    if platform.system() == "Darwin":
+        """
         Interesting way to get res on macos
-        '''
-        res = process('system_profiler SPDisplaysDataType | grep Resolution'
-        ).split('Resolution: ')[1].strip()
+        """
+        res = (
+            process("system_profiler SPDisplaysDataType | grep Resolution")
+            .split("Resolution: ")[1]
+            .strip()
+        )
 
-    elif platform.system() == 'Linux':
-        '''If this doesn't work on your distro, let me know and i'll fix it.'''
-        res = process(
-            'xrandr | grep "\*" | cut -d" " -f4'
-            ).strip()
+    elif platform.system() == "Linux":
+        """If this doesn't work on your distro, let me know and i'll fix it."""
+        res = process('xrandr | grep "\*" | cut -d" " -f4').strip()
     return res
 
+
 def get_uptime():
-    '''u = subprocess.Popen(
+    """u = subprocess.Popen(
         'uptime',
         shell=True,
         stdout=subprocess.PIPE
-    ).communicate()[0].decode('utf-8').replace("up ", "")'''
-    if platform.system() == 'Linux':
-        return process('uptime -p').replace("up ","")
-    return ' '.join([i.strip() for i in process(
-        'uptime'
-        ).replace("up ", "").split(',')[:3]])
+    ).communicate()[0].decode('utf-8').replace("up ", "")"""
+    if platform.system() == "Linux":
+        return process("uptime -p").replace("up ", "")
+    return " ".join(
+        [i.strip() for i in process("uptime").replace("up ", "").split(",")[:3]]
+    )
+
 
 def detect_desktop_environment():
-    desktop_environment = 'generic'
-    if os.environ.get('KDE_FULL_SESSION') == 'true':
-        desktop_environment = 'kde'
-    elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
-        desktop_environment = 'gnome'
-    elif platform.system() == 'Darwin':
-        desktop_environment = 'Aqua'
+    desktop_environment = "generic"
+    if os.environ.get("KDE_FULL_SESSION") == "true":
+        desktop_environment = "kde"
+    elif os.environ.get("GNOME_DESKTOP_SESSION_ID"):
+        desktop_environment = "gnome"
+    elif platform.system() == "Darwin":
+        desktop_environment = "Aqua"
     else:
         try:
-            info = subprocess.check_output('xprop -root _DT_SAVE_MODE')
+            info = subprocess.check_output("xprop -root _DT_SAVE_MODE")
             if ' = "xfce4"' in info:
-                desktop_environment = 'xfce'
+                desktop_environment = "xfce"
         except (OSError, RuntimeError):
             pass
     return desktop_environment
 
+
 def get_user():
-    username = os.environ['USER']
-    hostname = process(
-            'hostname',
-        ).strip().rstrip('\n')
-    return f'{username}@{hostname}'
+    username = os.environ["USER"]
+    hostname = (
+        process(
+            "hostname",
+        )
+        .strip()
+        .rstrip("\n")
+    )
+    return f"{username}@{hostname}"
+
 
 def get_system_info():
-    '''
+    """
     Layout of get_system_info
 
     OS: MacOS 11.4
@@ -80,26 +91,26 @@ def get_system_info():
     CPU: intel m3 (speed)
     GPU: intel graphics 616
     Memory: xyz/XYZmb
-    '''
-    info={}
+    """
+    info = {}
     # Hacky and could be better
-    info['User'] = get_user()
-    info['Hostname']=socket.gethostname()
-    info['Uptime'] = get_uptime()
-    info['OS']=platform.platform(aliased=True).split('-')[0]
-    info['Kernel'] = platform.release()
-    info['Shell'] = os.environ['SHELL'].split('/')[-1]
-    info['Resolution'] = grab_res()
-    info['DE/WM']=detect_desktop_environment()
-    info['Architecture']=platform.machine()
-    info['Terminal']= os.environ['TERM']
-    info['Processor']=platform.processor()
+    info["User"] = get_user()
+    info["Hostname"] = socket.gethostname()
+    info["Uptime"] = get_uptime()
+    info["OS"] = platform.platform(aliased=True).split("-")[0]
+    info["Kernel"] = platform.release()
+    info["Shell"] = os.environ["SHELL"].split("/")[-1]
+    info["Resolution"] = grab_res()
+    info["DE/WM"] = detect_desktop_environment()
+    info["Architecture"] = platform.machine()
+    info["Terminal"] = os.environ["TERM"]
+    info["Processor"] = platform.processor()
 
     try:
-        import psutil 
+        import psutil
     except ModuleNotFoundError:
         pass
     finally:
-        info['RAM']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
-        
+        info["RAM"] = str(round(psutil.virtual_memory().total / (1024.0**3))) + " GB"
+
     return info
